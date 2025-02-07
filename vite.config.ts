6f4051b773c4f9f725d9ceb1c53e6e1749ae998b
Terry <terry_su@fittech.com.tw>
2025-02-07 16:42:00 +0800

    Updated files for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://terrysu418.github.io/Frontend-Mentor-Social-links-profile",
   plugins: [react(), tailwindcss(),],
   
 })
